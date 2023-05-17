d37f1f524613b283fc0c31c6c799057eb70c27db
pixaily <danield.georgiev@gmail.com>
2023-05-17 10:24:05 +0300

    To Do App - Advanced - Working with local storage

--- todo-app/advanced/assets/js/script.js
+++ todo-app/advanced/assets/js/script.js
@@ -2,37 +2,59 @@ const addForm = document.querySelector('#todo-add-from')
 const addField = document.querySelector('#todo-add-field')
 const todoList = document.querySelector('#todo-list')
 const itemTemplate = document.querySelector('#todo-item-template')
-const items = [];
+const TODO_APP_PREFIX = 'todo_'
+const items = {};
+const itemsObj = {};
 
 // 1. On form sumbit to prepare data
 
-(function initialLoad() {
-  const storage = JSON.parse(localStorage.getItem('todoList'))
-  if (storage) {
-    storage.forEach(item => createNewTodoItem(item))
+const storage = JSON.parse(localStorage.getItem(TODO_APP_PREFIX + 'list'))
+
+if (storage && Object.keys(storage).length > 0) {
+  for (const [key, value] of Object.entries(storage)) {
+    createNewTodoItem(value)
   }
-})()
+}
 
 addForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const title = addField.value;
+  const isChecked = false;
 
   if (title.length > 0) {
-    items.push(title);
-    createNewTodoItem(title);
+    createNewTodoItem({ title, isChecked });
   }
-
 
   addField.value = '';
 })
 
+todoList.addEventListener('click', function (e) {
+  if (e.target.matches('[data-todo-item-checkbox]')) {
+    const title = e.target.nextElementSibling.textContent;
+    const curItem = items[prepareTheKey(title)];
+
+    curItem.isChecked = !curItem.isChecked;
+
+    updateStorage();
+  }
+})
+
 // 2. Prepare the new element and append it
 
-function createNewTodoItem(title) {
+function createNewTodoItem(item) {
+  items[prepareTheKey(item.title)] = {
+    title: item.title,
+    isChecked: item.isChecked
+  }
   const newItem = itemTemplate.content.cloneNode(true)
-  newItem.querySelector('[data-todo-item-text]').textContent = title;
+  newItem.querySelector('[data-todo-item-text]').textContent = item.title;
+  const checkbox = newItem.querySelector('[data-todo-item-checkbox]');
 
+  checkbox.checked = item.isChecked;
+  if (item.isChecked) {
+    newItem.querySelector('li').classList.add('finished');
+  }
   updateStorage();
 
   registerItemEvents(newItem.querySelector('.todo-item'));
@@ -43,7 +65,7 @@ function createNewTodoItem(title) {
 // 3. Save it in the storage
 
 function updateStorage() {
-  localStorage.setItem('todoList', JSON.stringify(items));
+  localStorage.setItem(TODO_APP_PREFIX + 'list', JSON.stringify(items));
 }
 
 // 4. Prepare the check and delete events
@@ -64,8 +86,11 @@ function deleteTodoItem(event) {
 
   currentItem.remove();
 
-  const index = items.indexOf(title);
-  items.splice(index, 1);
+  delete items[prepareTheKey(title)];
 
   updateStorage();
+}
+
+function prepareTheKey(title) {
+  return title.split(' ').join('');
 }
